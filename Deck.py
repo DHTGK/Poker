921bb3cf7c68c4aa0b23d00463baacc5eb358c61
DHTGK <aliu144@ucr.edu>
2026-03-26 14:58:17 -0700

    A functional deck with card counting.

--- Deck.py
+++ Deck.py
@@ -6,56 +6,60 @@ class Card:
         self.number = number
     
     def print(self):
-        if self.suite == 0:
-            suite = "Club"
-        elif self.suite == 1:
-            suite = "Heart"
-        elif self.suite == 2:
-            suite = "Spade"
-        elif self.suite == 3:
-            suite = "Diamond"
-        
-        if self.number == 1:
-            number = "Ace"
-        elif self.number == 11:
-            number = "Jack"
-        elif self.number == 12:
-            number = "Queen"
-        elif self.number == 13:
-            number = "King"
-        else:
-            number = str(self.number)
-
-        print(suite, number)
+        print(self.suite, self.number)
 
 class Deck:
-    deck = [1] * 52
-    count = 52
+    deck = []
+    drawn_cards = []
+
+    def __init__(self):
+        #make a full sized deck of cards
+        for x in range (0, 4):
+            for y in range (0, 13):
+                if y == 0:
+                    number = "Ace"
+                elif y == 10:
+                    number = "Jack"
+                elif y == 11:
+                    number = "Queen"
+                elif y == 12:
+                    number = "King"
+                else:
+                    number = str(y + 1)
+
+                if x == 0:
+                    suite = "Spade"
+                elif x == 1:
+                    suite = "Heart"
+                elif x == 2:
+                    suite = "Club"
+                elif x == 3:
+                    suite = "Diamond"
+
+                self.deck.append(Card(number, suite))
+                
 
     def draw(self):
-        card = random.randint(0, 51)
+        if len(self.deck) == 0:
+            print("No Cards")
+            return
+        draw_number = random.randint(0, len(self.deck) - 1)
 
-        suite = math.floor(card / 13)
-        card_number = (card - (suite * 13)) + 1
+        card = self.deck[draw_number]
+        self.deck.pop(draw_number)
+        
+        self.drawn_cards.append(card)
 
-        return Card(suite, card_number)
+        card.print()
 
     def check(self):
-        for x in range(0, 52):
-            card = x
-
-            suite = math.floor(card / 13)
-            card_number = (card - (suite * 13)) + 1
-            
-            current_card = Card(suite, card_number)
+        for x in range(0, len(self.deck)):
+            current_card = self.deck[x]
             
             current_card.print()
     
     def reset(self):
-        for x in self.deck:
-            x = 1
-        count = 52
+        self.deck = self.deck + self.drawn_cards
+        self.drawn_cards.clear()
 
 Poker = Deck()
-Poker.check()
-Poker.reset()
